c069558976f2f003c6aed17ba81e5b070a25b438
Brian <brian.hardy67@gmail.com>
2022-02-05 16:10:57 -0500

    Recipe Name Comp boilerplate completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import {MatCardModule} from '@angular/material/card';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
-    MatCardModule
+    MatCardModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/recipe-main/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe-main/recipe-detail/recipe-detail.component.ts
@@ -6,9 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-detail.component.scss']
 })
 export class RecipeDetailComponent implements OnInit {
-
+  panelOpenState = false;
   constructor() { }
 
+
+  onEditRecipe() {
+    console.log('edit recipe');
+  }
+
+  onDeleteRecipe() {
+    console.log('delete recipe');
+  }
+  
   ngOnInit(): void {
   }
 
