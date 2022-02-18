dc33a9d7e583c80ed426172131211ecc97e59dfc
Brian <brian.hardy67@gmail.com>
2022-02-18 14:15:56 -0500

    added disabled header buttons if not auth

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,25 +1,42 @@
-import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 import { DataStorageService } from '../shared/data-storage.service';
 
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
-@Input('class') panelClass!: string;
-  constructor(private storeDataService: DataStorageService) { }
+export class HeaderComponent implements OnInit, OnDestroy {
+  @Input('class') panelClass!: string;
+  isAuthenticated: boolean = false;
+  private userSub!: Subscription
+  constructor(
+    private storeDataService: DataStorageService,
+    private authService: AuthService
+  ) { }
 
-  saveData(){
+  saveData() {
     this.storeDataService.storeRecipes();
   }
 
-  fetchData(){
+  fetchData() {
     this.storeDataService.fetchRecipes().subscribe();
   }
 
   ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe(user => {
+     this.isAuthenticated = !!user;
+    }
+    );
   }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
+  }
+
+
 
 }
